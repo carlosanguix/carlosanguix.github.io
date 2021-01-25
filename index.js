d62e1d991fee2115465069d05a880ca0a4411d5f
carlosanguix <carlos.anguix@gmail.com>
2021-01-25 18:12:40 +0100

    background completed, new functionality, fighting with projects ...

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 
+function scrolls() {
+
+    let projects = document.querySelectorAll('.project');
+    let count = 0;
+    projects.forEach(project => {
+        count++;
+        let box = project.getBoundingClientRect();
+        if ((box.y + box.height - 150) <= window.innerHeight) {
+            project.classList.add('projectVisible');
+            
+        }
+    });
+}
+
 function init() {
-   
+    
+    window.onscroll = () => {
+        scrolls();
+    }
+    
 }
 
 
