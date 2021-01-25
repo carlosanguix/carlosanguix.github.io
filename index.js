061fb6638f224387ba571b4469879cf3f2102dd6
carlosanguix <carlos.anguix@gmail.com>
2021-01-25 11:13:49 +0100

    changing things

--- index.js
+++ index.js
@@ -1,16 +1,6 @@
 
 function init() {
-    document.querySelector('#contactButton').onclick = () => {
-        document.querySelector('#contactBox').classList = 'contactBoxIn';
-        document.querySelector('#index1').style.display = 'none';
-        document.querySelector('#background').classList = 'backgroundBlur';
-    };
-
-    document.querySelector('#contactBox').querySelector('.close').onclick = async() => {
-        document.querySelector('#contactBox').classList = 'contactBoxOut';
-        document.querySelector('#index1').style.display = 'flex';
-        document.querySelector('#background').classList.remove('backgroundBlur');
-    };
+   
 }
 
 
