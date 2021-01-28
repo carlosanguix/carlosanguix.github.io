daeb41ba59e860697c60cb08744698acf0fb899a
carlosanguix <carlos.anguix@gmail.com>
2021-01-28 13:21:09 +0100

    added clipath and reforming styles

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 
+function linkAnimations() {
+
+    let links = document.querySelectorAll('.link');
+    for (let i = 0; i < links.length; i++) {
+        links[i].addEventListener('mouseover', () => {
+            links[i].children[0].classList.add('linkHover');
+        });
+        links[i].addEventListener('mouseout', () => {
+            links[i].children[0].classList.remove('linkHover');
+        });
+    }
+}
+
 function addScrollAnimations() {
 
+    // ProjectBoxes animations
     let projects = document.querySelectorAll('.project');
-
     for (let i = 0; i < projects.length; i++) {
         let box = projects[i].getBoundingClientRect();
         if ((box.y + box.height) <= window.innerHeight) {
@@ -10,6 +23,7 @@ function addScrollAnimations() {
         }
     }
 
+    // H1 and lines animations
     let boxes = document.querySelectorAll('.box');
     for (let i = 0; i < boxes.length; i++) {
         let box = boxes[i].firstElementChild.getBoundingClientRect();
@@ -18,26 +32,39 @@ function addScrollAnimations() {
             boxes[i].children[1].classList.add('revealLeft');
         }
     }
+
+    //
+    let aboutDiv = document.querySelector('#aboutDiv');
+    let photo = aboutDiv.children[2].children[0];
+    let photoBound = photo.getBoundingClientRect();
+    let descript = aboutDiv.children[2].children[1];
+    let descriptBound = descript.getBoundingClientRect();
+    if ((photoBound.y + photoBound.height + 100) <= window.innerHeight) {
+        photo.classList.add('revealLeft');
+    }
+    if ((descriptBound.y + descriptBound.height + 70) <= window.innerHeight) {
+        descript.classList.add('revealRight34');
+    }
 }
 
 function fillTheLanguages() {
 
     let languagesDiv = document.querySelector('#languages');
     for (let i = 0; i < languages.length; i++) {
-        console.log(languages[i]);
         languagesDiv.innerHTML += "<span id=" + languages[i] + ">" + languages[i] + "</span>";
     }
 }
 
 function init() {
-    
+
+    linkAnimations();
+
+
     window.onscroll = () => {
         addScrollAnimations();
     }
 
-    fillTheLanguages();
-    
-   
+
     
 }
 
